7031f77655189acfc629081432629424804973b1
Karl Cardenas <kcardenas@hashicorp.com>
2021-12-10 10:34:31 -0700

    fix: fixed missing closing tick-v2

--- docker-compose-consul/chaos.sh
+++ docker-compose-consul/chaos.sh
@@ -9,7 +9,7 @@ curl 'http://localhost:8080/api' \
       -H 'Connection: keep-alive' \
       -H 'DNT: 1' \
       -H 'Origin: http://localhost:8080' \
-      --data-binary '{"query":"mutation{ pay(details:{ name: \"nic\", type: \"mastercard\", number: \"1234123-0123123\", expiry:\"10/02\",    cv2: 1231, amount: 12.23 }){id, card_plaintext, card_ciphe$'
+      --data-binary '{"query":"mutation{ pay(details:{ name: \"nic\", type: \"mastercard\", number: \"1234123-0123123\", expiry:\"10/02\",    cv2: 1231, amount: 12.23 }){id, card_plaintext, card_ciphertext, message } }"}' --compressed
 
  if [ $? != 0 ]; then
         echo "ERROR: Failed connecting to the API"
