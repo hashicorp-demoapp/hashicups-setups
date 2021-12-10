419e4cf54d0e527ba6eb4f2e3d7b06861204b87f
Karl Cardenas <kcardenas@hashicorp.com>
2021-12-10 09:57:17 -0700

    fix: added a script to query public-api

---
+++ docker-compose-consul/chaos.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+query() {
+
+curl 'http://localhost:8080/api' \
+      -H 'Accept-Encoding: gzip, deflate, br' \
+      -H 'Content-Type: application/json' \
+      -H 'Accept: application/json' \
+      -H 'Connection: keep-alive' \
+      -H 'DNT: 1' \
+      -H 'Origin: http://localhost:8080' \
+      --data-binary '{"query":"mutation{ pay(details:{ name: \"nic\", type: \"mastercard\", number: \"1234123-0123123\", expiry:\"10/02\",    cv2: 1231, amount: 12.23 }){id, card_plaintext, card_ciphe$
+
+ echo " "
+ if [ $? != 0 ]; then
+        echo "ERROR: Failed connecting to the API"
+ fi
+}
+
+
+COUNTER=0
+seq 5 | while true; do
+
+query
+
+COUNTER=$[$COUNTER + 1]
+echo "Interation: $COUNTER"
+echo '####################################'
+sleep 1
+
+done
